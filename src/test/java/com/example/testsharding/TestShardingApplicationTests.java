5657c0b2c78a2d70de3c3a29c93c8d8978c7dcd3
hzhang <1049725878@qq.com>
2023-09-06 16:46:05 +0800

    test SINGLE

--- src/test/java/com/example/testsharding/TestShardingApplicationTests.java
+++ src/test/java/com/example/testsharding/TestShardingApplicationTests.java
@@ -5,15 +5,36 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @SpringBootTest
 class TestShardingApplicationTests {
 
+//    @Resource
+//    EquipmentOeeService equipmentOeeService;
+//
+//    @Test
+//    public void test01() {
+//        equipmentOeeService.test01();
+//    }
+
+
     @Resource
-    EquipmentOeeService equipmentOeeService;
+    DataSource dataSource;
 
     @Test
-    public void test01() {
-        equipmentOeeService.test01();
+    public void test02() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            connection.prepareStatement("select * from equipment_oee").execute();
+            connection.prepareStatement("select * from equipment_status").execute();
+        }
+        // result
+        // ShardingSphere-SQL                       : Logic SQL: select * from equipment_oee
+        // ShardingSphere-SQL                       : Actual SQL: aa ::: select * from equipment_oee
+        // ShardingSphere-SQL                       : Logic SQL: select * from equipment_status
+        // ShardingSphere-SQL                       : Actual SQL: aa ::: select * from equipment_status
     }
+
 
 }
